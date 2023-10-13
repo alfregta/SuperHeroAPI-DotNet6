2b7edd811ae4bd6d00ee905d898104216bcbafee
alfregta <alfregt15243@gmail.com>
2023-10-12 23:39:35 -0400

    Add project files.

---
+++ SuperHeroAPI/Services/SuperHeroesService.cs
@@ -0,0 +1,27 @@
+﻿using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Services
+{
+    public class SuperHeroesService
+    {
+
+        /*
+        public List<SuperHero> superHeroes()
+        {
+            return null;
+        }
+
+        public SuperHero GetSuperHeroById() 
+        {
+            return null;
+        }
+
+
+        public List<SuperHero> AddSuperHero()
+        {
+            
+        }
+
+        */
+    }
+}
